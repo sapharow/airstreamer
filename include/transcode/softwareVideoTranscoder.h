ef41bb2e9a3f86dae9ee70c3daff42510ce16ae3
Iskandar Safarov <sapharow@gmail.com>
2017-02-21 14:21:05 +0500

    Hidden libav implementation

--- include/transcode/softwareVideoTranscoder.h
+++ include/transcode/softwareVideoTranscoder.h
@@ -1,67 +1,18 @@
 #pragma once
 #include <transcode/videoTranscoder.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavfilter/avfiltergraph.h>
-#include <libavfilter/buffersink.h>
-#include <libavfilter/buffersrc.h>
-#include <libavutil/opt.h>
-#include <libavutil/pixdesc.h>
-
-
-#ifdef __cplusplus
-}
-#endif
-
 namespace fp {
 	namespace trans {
 
-#if 0
-		/**
-		 * Transcoder class
-		 */
-		class SoftwareVideoTranscoder : public Transcoder {
-		public:
-
-			/**
-			 * Create transcoder with output
-			 */
-			SoftwareVideoTranscoder(StreamType inputType, 
-			                   		const StreamRef& output);
-			~SoftwareVideoTranscoder() override;
-
-			void supplyFrame(const uint8_t* data, size_t size, Stream::Metadata*) override;
-			void reset() override;
-
-		private:
-			AVCodecContext* m_AVInputCodecContext = nullptr;
-			AVCodecContext* m_AVOutputCodecContext = nullptr;
-
-			std::vector<AVFrame*> m_DecodedFramePool;
-			AVFrame* m_DecodedFrame;
-			size_t m_DecodedFramePoolSize = 0;
-
-			uint64_t m_ExpectedPTS;
-			size_t findFrameWithPTS(int64_t);
-			void insertFrame();
-			void initialiseEncoder(uint32_t width, uint32_t height);
-		};
-#else
-		DECLARE_CLASS(SoftwareDecodedFrame);
 		class SoftwareDecodedFrame : public DecodedFrame {
 		public:
 			SoftwareDecodedFrame();
 			~SoftwareDecodedFrame() override;
 			bool operator <(const DecodedFrame& reference) override;
 
-			AVFrame* rawFrame();
+			void* rawFrame();
 		private:
-			AVFrame* m_Frame = nullptr;
+			void* m_Frame = nullptr;
 		};
 
 		class SoftwareDecoderContext : public DecoderContext {
@@ -70,7 +21,7 @@ namespace fp {
 			~SoftwareDecoderContext() override;
 			size_t decodeFrame(const uint8_t*, size_t, Stream::Metadata*, const DecodedFrameRef&) override;
 		private:
-			AVCodecContext* m_Decoder = nullptr;
+			void* m_Decoder = nullptr;
 		};
 
 		class SoftwareEncoderContext : public EncoderContext {
@@ -79,7 +30,7 @@ namespace fp {
 			~SoftwareEncoderContext() override;
 			size_t encodeFrame(const DecodedFrameRef&, std::vector<uint8_t>&) override;
 		private:
-			AVCodecContext* m_Encoder = nullptr;
+			void* m_Encoder = nullptr;
 		};
 
 		/**
@@ -93,6 +44,6 @@ namespace fp {
 			EncoderContextRef createEncoder(const DecodedFrameRef&) override;
 			DecodedFrameRef allocateFrame() override;
 		};
-#endif
+
 	}
 }
